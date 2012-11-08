630392816a054039bce754f6cc1164d73e7b6a29
Jonathan Wilsson <jonathan@supernormal.se>
2012-11-08 17:56:21 +0100

    more bugfixes and code cleanup

--- jquery.yaselect.js
+++ jquery.yaselect.js
@@ -9,160 +9,190 @@
 * Modified by Supernormal
 */
 
-(function ($) {
-	$.fn.yaselect = function (config) {
+(function ( $ ) {
+	$.fn.yaselect = function ( config ) {
 		var defaults = {
-			hoverOnly: false,
-			size: 5,
 			individualStyling: true,
-			useSelectCss: {
-				reuseWidth: true,
-				addWidthToOriginal: 0
-			}
+			size: 5,
+			reuseWidth: true
 		};
 
 		config = $.extend(defaults, config);
 
-		function getText(jselect) {
+		function getText ( jselect ) {
 			var text = '',
 				selectedIndex = 0;
 
-			if (jselect[0].selectedIndex) {
+			if ( jselect[0].selectedIndex ) {
 				selectedIndex = jselect[0].selectedIndex;
 			}
 
-			text = jselect.find('option:nth(' + selectedIndex + ')').text();
+			text = jselect.find( 'option:nth(' + selectedIndex + ')' ).text();
 
 			return text;
 		}
 
-		function setText(jselect, curr) {
-			var text = getText(jselect);
+		function setText ( jselect, curr ) {
+			var text = getText( jselect );
 
-			if (text === '') {
-				text = jselect.children( 'option:selected' ).text() + '';
+			if ( text === '' ) {
+				text = jselect.children( 'option:selected' ).text();
 			}
 
 			text = text + '<div class="yaselect-right-image"></div>';
 
-			curr.html(text);
+			curr.html( text );
 		}
 
-		function setValue(jselect, curr, wrap, to_focus_wrap) {
+		function setValue ( jselect, curr, wrap, focusWrap ) {
 			jselect.blur();
 
-			setText(jselect, curr);
+			setText( jselect, curr );
 
-			/*jselect.css({
-				top: wrap.outerHeight()
-			});*/
-
-			if (to_focus_wrap) {
+			if ( focusWrap ) {
 				wrap.focus();
 			}
 		}
 
-		function toggleOpen(wrap) {
-			wrap.toggleClass('yaselect-open yaselect-close');
+		function toggleOpen ( wrap ) {
+			wrap.toggleClass( 'yaselect-open yaselect-close' );
 		}
 
-		return this.each(function (index, select) {
-			var jselect = $(select).addClass('yaselect-select'),
-				wrap = $('<div class="yaselect-wrap yaselect-open"><div class="yaselect-current"></div></div>'),
-				anchor = wrap.wrap('<div class="yaselect-anchor"></div>').parent(),
-				curr = wrap.find('.yaselect-current');
+		function isMobile () {
+			return navigator.userAgent.match( /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i );
+		}
+
+		return this.each( function ( index, select ) {
+			var jselect = $( select ).addClass( 'yaselect-select' ),
+				wrap = $( '<div class="yaselect-wrap yaselect-open"><div class="yaselect-current"></div></div>' ),
+				anchor = wrap.wrap( '<div class="yaselect-anchor"></div>' ).parent(),
+				curr = wrap.find( '.yaselect-current' ),
+				width = 0,
+				options = jselect.find( 'option' ),
+				selectedOption = jselect[0].selectedIndex,
+				fontSize;
 
 			setText(jselect, curr);
 
-			if (config.individualStyling) {
-				jselect.addClass( 'yaselect-wrap-' + index );
+			if ( config.individualStyling ) {
+				wrap.addClass( 'yaselect-wrap-' + index );
 			}
 
-			if (config.hoverOnly || window.navigator && navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry/i)) {
-				toggleOpen(wrap);
+			if ( isMobile() ) {
+				toggleOpen( wrap );
 
-				return jselect /* becomes invisible and is placed above wrapper to receive screen tap -- triggering native <select> */
-					.before(anchor)
-					.css("opacity", 0.001)
-					.on("change", function () {
-						setText(jselect, curr);
+				// Becomes invisible and is placed above wrapper to receive screen tap -- triggering native <select>
+				jselect
+					.before( anchor )
+					.css({
+						opacity: 0.001,
+						left: 0,
+						top: 0,
+						zIndex: 10
+					})
+					.on( 'change', function () {
+						setText( jselect, curr );
+					})
+					.appendTo( anchor );
+			} else {
+				jselect
+					.before( anchor )
+					.on( 'keydown', function ( e ) {
+						if (e.keyCode === 13 || e.keyCode === 32) {
+							e.preventDefault();
+
+							setValue( jselect, curr, wrap, true );
+						}
+					})
+					.on( 'change', function () {
+						setText( jselect, curr );
+					})
+					.on( 'blur', function () {
+						jselect
+							.css( 'z-index', 0 )
+							.hide();
+
+						toggleOpen( wrap );
+					})
+					.on( 'click', function () {
+						if ( jselect.is( ':hidden' ) ) {
+							toggleOpen( wrap );
+
+							jselect
+								.css( 'z-index', 1 )
+								.show();
+
+							setTimeout( function () {
+								jselect.focus();
+							}); // Avoid trampling confusion with triggered blur
+						} else {
+							setValue( jselect, curr, wrap, true );
+						}
 					})
 					.appendTo(anchor);
 			}
 
-			jselect
-				.before(anchor)
-				.on("keydown", function (e) {
-					if (e.keyCode == 13 || e.keyCode == 32) {
-						e.preventDefault();
-
-						setValue(jselect, curr, wrap, true);
-					}
-				})
-				.on("change", function () {
-					setText(jselect, curr);
-				})
-				.on("blur", function () {
-					jselect.hide();
-
-					toggleOpen(wrap);
-				})
-				.on("click", function () {
-					if (jselect.is(':hidden')) {
-						toggleOpen(wrap);
-
-						jselect.show();
-
-						setTimeout(function () {
-							jselect.focus();
-						}); /* avoid trampling confusion with triggered blur */
-					} else {
-						setValue(jselect, curr, wrap, true);
-					}
-				})
-				.appendTo(anchor);
-
 			wrap
-				.on("mousedown", function () {
+				.on( 'mousedown', function () {
 					jselect.click();
 				})
-				.on("keydown", function (e) {
+				.on( 'keydown', function ( e ) {
 					var keys = [13, 32, 37, 38, 39, 40];
 
-					if ($.inArray(e.keyCode, keys) !== -1) {
+					// Avoid pagescroll
+					if ( $.inArray( e.keyCode, keys ) !== -1 ) {
 						e.preventDefault();
 
 						jselect.click();
 					}
-				}); /* preventDefault avoid pagescroll */
+				});
 
-			if (config.useSelectCss.reuseWidth) {
-				var width = 0,
-					options = jselect.find("option");
-
-				options.each(function () {
+			if ( config.reuseWidth ) {
+				options.each( function () {
 					var optionWidth = 0;
 
-					options.removeAttr("selected");
+					options
+						.removeAttr( 'selected' );
 
-					$(this).attr("selected", "selected");
+					$( this )
+						.attr( 'selected', 'selected' );
 
-					setText(jselect, curr);
+					setText( jselect, curr );
 
 					optionWidth = anchor.outerWidth();
 
-					if (optionWidth > width) {
+					if ( optionWidth > width ) {
 						width = optionWidth;
 					}
 				});
 
-				width += $(".yaselect-right-image").outerWidth();
+				options
+					.removeAttr( 'selected' )
+					.eq( selectedOption )
+					.attr( 'selected', 'selected' );
 
-				anchor.width(width + config.useSelectCss.addWidthToOriginal);
+				width += wrap.find( '.yaselect-right-image' ).outerWidth();
+
+				anchor.width( width );
+			}
+
+			if ( isMobile() ) {
+				jselect
+					.css( 'width', anchor.width() );
+
+				// Android won't set height so we'll increase the font size instead
+				while ( jselect.outerHeight() < anchor.outerHeight() ) {
+					fontSize = parseInt( jselect.css( 'fontSize' ), 10 );
+
+					jselect.css( 'fontSize', fontSize + 1 + 'px' );
+				}
+
+				// We're resetting it here to prevent height issues on Android and it's never used so it's OK
+				config.size = 1;
 			}
 
 			select.size = config.size;
-			setValue(jselect, curr, wrap, false);
+			setValue( jselect, curr, wrap, false );
 		});
 	};
-})(jQuery);
+}( jQuery ) );
